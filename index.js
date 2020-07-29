e2a6558a266738460f24f299d2de28312e17d304
Bennett Comendul <58659369+BComendul@users.noreply.github.com>
2020-07-28 17:59:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+File to Display 4th down data using Kaggle Dataset
+
+Vivid description of situation-- Coach coming up with new stretegy for the year
+As the assistant coach you've got an idea to present to your boss-
+  Statistical analysis of when you should kick the FG and when you should run a play on 4th
+
+Talk about dataset- Kaggle.com 2009 to 2018 every single NFL play included
+include numrows and num columns
+Talk about process and using CLT- central limit theorum
+Going to use the data to give an accurate model for FG and one play success rates
+
+*/
